6143b7c09a8316aaf925e6e2965e36ddc4533e02
Tauseef Khan <tauseefk@gmail.com>
2017-12-27 16:09:41 -0500

    added more walls

---
+++ Assets/SphereBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehaviour : MonoBehaviour {
+
+	Rigidbody _rb;
+	// Use this for initialization
+	void Start () {
+		_rb = GetComponent<Rigidbody> ();
+		_rb.AddForce (new Vector3(0, 0, 2000));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
